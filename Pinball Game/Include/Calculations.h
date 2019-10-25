8d6b1a5052960a14c67fe1881aa5ae07796b4165
Cabbage <kiwibirdi@hotmail.com>
2019-10-25 11:46:45 +0200

    fixed duplicate .h file

--- Pinball Game/Include/Calculations.h
+++ Pinball Game/Include/Calculations.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef CALCULATIONS_H_INCLUDED
+#define CALCULATIONS_H_INCLUDED
+#include <math.h>
+
+struct Calculations
+{
+	Vector2 Normalize(Vector2 A);		//make the vector 1 in magnitude
+	float Dot(Vector2 A, Vector2 B);	//find the dot product of 2 vectors
+};
+
+struct Vector2
+{
+	float x;
+	float y;
+};
+
+#endif // !CALCULATIONS_H_INCLUDED
